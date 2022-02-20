077724b54335febaeab9237dec3afcde08b1c802
soumik9876 <soumik9876@gmail.com>
2022-02-20 22:58:19 +0600

    Base model and teacher,student models implemented

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,31 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext as _
+
+from core.models import BaseModel
 
 
 class User(AbstractUser):
     pass
+
+
+class Teacher(BaseModel):
+    user = models.OneToOneField(User, verbose_name=_("User"), on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Teacher"
+        verbose_name_plural = "Teachers"
+
+    def __str__(self):
+        return self.user.username
+
+
+class Student(BaseModel):
+    user = models.OneToOneField(User, verbose_name=_("User"), on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Student"
+        verbose_name_plural = "Students"
+
+    def __str__(self):
+        return self.user.username
--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class BaseModel(models.Model):
+    """
+        Base model for inheriting common fields
+    """
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
