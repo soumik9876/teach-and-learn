3c9f5351ac1d0581bf7e78e65e95741e6db3064c
Saurav-Paul <sauravpaul.sunny@gmail.com>
2022-02-27 17:29:22 +0600

    CourseCategory serializer created

--- course/api/v1/serializers.py
+++ course/api/v1/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
-from course.models import Course
+from course.models import Course, CourseCategory
 
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = '__all__'
+
+
+class CourseCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CourseCategory
