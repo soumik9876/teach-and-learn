8857a7c601d129b35c360d93a6e79061d9dbbad2
Saurav-Paul <sauravpaul.sunny@gmail.com>
2022-03-10 12:43:25 +0600

    serializers updated

--- course/api/v1/serializers.py
+++ course/api/v1/serializers.py
@@ -3,10 +3,16 @@ from rest_framework import serializers
 from course.models import Course, CourseCategory, Video, Blog, Comment
 
 
+# noinspection PyMethodMayBeStatic
 class CourseSerializer(serializers.ModelSerializer):
+    category_name = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_category_name(self, obj):
+        return obj.category.title
 
 
 class CourseCategorySerializer(serializers.ModelSerializer):
@@ -31,4 +37,3 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
-
