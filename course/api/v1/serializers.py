1dd3c7226c5ff10e9466c3ba5763cf2ba95f972c
soumik9876 <soumik9876@gmail.com>
2022-03-10 18:30:50 +0600

    course serializer student and teacher field updated

--- course/api/v1/serializers.py
+++ course/api/v1/serializers.py
@@ -1,19 +1,29 @@
 from rest_framework import serializers
 
+from accounts.api.v1.serializers import TeacherSerializer, StudentSerializer
 from accounts.models import Teacher, Student
 from course.models import Course, CourseCategory, Video, Blog, Comment
 
 
 # noinspection PyMethodMayBeStatic
 class CourseSerializer(serializers.ModelSerializer):
-    teacher = serializers.PrimaryKeyRelatedField(many=True, queryset=Teacher.objects.all())
-    student = serializers.PrimaryKeyRelatedField(many=True, queryset=Student.objects.all(), required=False)
+    teacher = serializers.PrimaryKeyRelatedField(many=True, queryset=Teacher.objects.all(), write_only=True)
+    teacher_list = serializers.SerializerMethodField(read_only=True)
+    student = serializers.PrimaryKeyRelatedField(many=True, queryset=Student.objects.all(), write_only=True,
+                                                 required=False)
+    student_list = serializers.SerializerMethodField(read_only=True)
     category = serializers.PrimaryKeyRelatedField(queryset=CourseCategory.objects.all(), required=False)
 
     class Meta:
         model = Course
         fields = '__all__'
         depth = 2
+
+    def get_teacher_list(self, obj):
+        return TeacherSerializer(obj.teacher.all(), many=True).data
+
+    def get_student_list(self, obj):
+        return StudentSerializer(obj.student.all(), many=True).data
 
 
 class CourseCategorySerializer(serializers.ModelSerializer):
